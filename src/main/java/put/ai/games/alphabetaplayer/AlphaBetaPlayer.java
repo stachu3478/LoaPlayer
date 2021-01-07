a5f78532363a45accd1a43095bf363e025fe7f22
stachu3478 <stachu3478@o2.pl>
2021-01-07 19:39:55 +0100

    Add support heuristic but sucks as main

--- src/main/java/put/ai/games/alphabetaplayer/AlphaBetaPlayer.java
+++ src/main/java/put/ai/games/alphabetaplayer/AlphaBetaPlayer.java
@@ -4,6 +4,7 @@
  */
 package put.ai.games.alphabetaplayer;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -64,21 +65,9 @@ public class AlphaBetaPlayer extends Player {
         b.doMove(m);
         indexer.fetch(b);
         // ranking code goes here
-        int ranking = groupState(getColor()) - groupState(getOpponent(getColor()));
+        int ranking = 0;//groupState(getColor()) - groupState(getOpponent(getColor()));
         b.undoMove(m);
         return ranking;
-    }
-
-    private int groupState(Color color) {
-        int state = 0;
-        for (int x = 0; x < indexer.getSize(); x++) {
-            for (int y = 0; y < indexer.getSize(); y++) {
-                if (color != indexer.getColor(x, y)) continue;
-                state -= 8;
-                state += indexer.getConnectable(color, x, y, getOpponent(getColor()));
-            }
-        }
-        return state;
     }
 
     public interface TimeWatchdog {
@@ -92,61 +81,130 @@ public class AlphaBetaPlayer extends Player {
         private final Board board;
         private int depth;
         private boolean isEckhausted;
-        private Move bestMove;
         private final Color me;
+        private BoardIndexer indexer;
 
         public AlphaBeta(Board board, Color me, TimeWatchdog watchdog) {
             this.board = board.clone();
             this.watchdog = watchdog;
             this.me = me;
+            this.indexer = new BoardIndexer(board);
         }
 
         public Move process() {
             List<Move> moves = board.getMovesFor(me);
-            this.bestMove = moves.get(0);
+            Move bestMove1 = moves.get(0);
             int depth = 1;
             try {
                 while (true) {
-
                     Move bestMove = moves.get(0);
                     isEckhausted = true;
+                    float bestValue = -1;
                     for (Move move : moves) {
-                        int bestValue = -1;
-                        int result = run(getOpponent(me), depth, -1, 1);
+                        float result = run(getOpponent(me), depth, -64 * board.getSize(), 64 * board.getSize());
                         if (result > bestValue) {
                             bestValue = result;
                             bestMove = move;
                         }
                     }
-                    this.bestMove = bestMove;
+                    System.out.println("Best val: " + bestValue);
+                    bestMove1 = bestMove;
                     if (isEckhausted) break;
                     depth++;
                 }
             } catch (TimeoutException e) {
                 System.out.println("Depth reached " + (depth - 1));
             }
-            return bestMove;
+            return bestMove1;
         }
 
-        private int run(Color color, int depth, int alpha, int beta)
+        private int groupState(Color color) {
+            indexer.fetch(board);
+            int state = 0;
+            for (int x = 0; x < indexer.getSize(); x++) {
+                for (int y = 0; y < indexer.getSize(); y++) {
+                    if (color != indexer.getColor(x, y)) continue;
+                    state -= 8;
+                    state += indexer.getConnectable(color, x, y, getOpponent(me));
+                }
+            }
+            return state;
+        }
+
+        private float run(Color color, int depth, float alpha, float beta)
         {
             if (!watchdog.call()) throw new TimeoutException();
             List<Move> moves = board.getMovesFor(color);
             if( moves.size() == 0 || depth == 0 ) {
-                if (depth == 0 && moves.size() != 0) isEckhausted = false;
+                if (moves.size() != 0) isEckhausted = false;
                 Color winner = board.getWinner(color);
-                if (winner == me) return 1;
-                if (winner == null) return 0;
-                return -1;
+                if (winner == color) return 64 * board.getSize();
+                if (winner == null) return new Labeller(board, color).run() - new Labeller(board, getOpponent(color)).run();
+                return -64 * board.getSize();
             }
             for(Move move : moves) {
                 board.doMove(move);
-                int val = -run(getOpponent(color), depth - 1, -beta, -alpha);
+                float val = -run(getOpponent(color), depth - 1, -beta, -alpha);
                 board.undoMove(move);
                 if( val > alpha ) alpha = val; // alpha=max(val,alpha);
                 if( alpha >= beta ) return beta; // cutoff
             } //endfor
             return alpha;
+        }
+
+        private static class Labeller {
+            private HashMap<Integer, Integer> labels = new HashMap<>();
+            private final Board board;
+            private final Color color;
+            private int labelled;
+            private int totalLabelled;
+
+            public Labeller(Board board, Color color) {
+                labels = new HashMap<>();
+                this.board = board;
+                this.color = color;
+                totalLabelled = 0;
+            }
+
+            public float run() {
+                int label = 0;
+                int maxLabelled = 0;
+                for (int x = 0; x < board.getSize(); x++) {
+                    for (int y = 0; y < board.getSize(); y++) {
+                        if (board.getState(x, y) == color && !labels.containsKey(x * board.getSize() + y)) {
+                            labelled = 1;
+                            totalLabelled++;
+                            putRecursiveLabel(label++, x, y);
+                            if (labelled > maxLabelled) maxLabelled = labelled;
+                        }
+                    }
+                }
+                if (maxLabelled == 0 || totalLabelled == 0) {
+                    System.out.println("Warning: No colors found on the board");
+                    return 0;
+                }
+                return (float)maxLabelled / (float)totalLabelled;
+            }
+
+            private void putRecursiveLabel(int label, int x, int y) {
+                checkAndPutLabel(label, x - 1, y);
+                checkAndPutLabel(label, x + 1, y);
+                checkAndPutLabel(label, x, y - 1);
+                checkAndPutLabel(label, x, y + 1);
+                checkAndPutLabel(label, x - 1, y - 1);
+                checkAndPutLabel(label, x - 1, y + 1);
+                checkAndPutLabel(label, x + 1, y - 1);
+                checkAndPutLabel(label, x + 1, y + 1);
+            }
+
+            private void checkAndPutLabel(int label, int x, int y) {
+                if (board.getState(x, y) == color && !labels.containsKey(x * board.getSize() + y)) {
+                    labels.put(x * board.getSize() + y, label);
+                    labelled++;
+                    totalLabelled++;
+                    putRecursiveLabel(label, x, y);
+                }
+            }
         }
     }
 
