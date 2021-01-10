a8d0c5fb9405ab2732aca79f66ffc26fa0a79864
stachu3478 <stachu3478@o2.pl>
2021-01-10 22:06:10 +0100

    Initial commit - no revelance

--- src/main/java/put/ai/games/alphabetaplayer/AlphaBetaPlayer.java
+++ src/main/java/put/ai/games/alphabetaplayer/AlphaBetaPlayer.java
@@ -45,65 +45,111 @@ public class AlphaBetaPlayer extends Player {
         private final Board board;
         private final Color me;
         private final Random random;
+        private MovePoller poller;
+        private Move bestMove;
+        private int depth;
 
         public AlphaBeta(Board board, Color me, TimeWatchdog watchdog, Random random) {
             this.board = board.clone();
             this.watchdog = watchdog;
             this.me = me;
             this.random = random;
+            this.poller = new MovePoller(me);
+            this.depth = 1;
         }
 
         public Move process() {
-            List<Move> moves = board.getMovesFor(me);
-            int depth = 0;
-            int movesRemoved = 0;
-            int movesTotal = moves.size();
+            depth = Math.max(1, depth - 3);
+            Move bestDepthMove = null;
+            this.poller.setMaxDepth(Integer.MAX_VALUE);
             try {
-                while (true) {
-                    List<Move> toBeRemoved = new ArrayList<>();
-                    for (Move move : moves) {
-                        board.doMove(move);
-                        float result = -run(getOpponent(me), depth, -1, 1);
-                        if (result == 1) return move; // winning
-                        else if (result == -1) {
-                            movesRemoved++; // losing
-                            toBeRemoved.add(move);
-                        }
-                        board.undoMove(move);
-                    }
-                    for (Move move : toBeRemoved) {
-                        moves.remove(move);
-                    }
-                    if (moves.size() == 1) return moves.get(0);
-                    if (moves.isEmpty()) return null;
+                while (watchdog.call()) {
+                    bestMove = null;
+                    float bestVal = run(me, depth, -Float.MAX_VALUE, Float.MAX_VALUE);
+                    System.out.println("Best val: " + bestVal);
+                    if (bestMove != null) bestDepthMove = bestMove;
+                    if (bestVal == Float.MAX_VALUE) return bestDepthMove;
                     depth++;
                 }
             } catch (TimeoutException e) {
                 System.out.println("Depth reached " + depth);
-                System.out.println("Moves available " + (movesTotal - movesRemoved) + " / " + movesTotal);
             }
-            return moves.get(random.nextInt(moves.size()));
+            if (bestDepthMove == null) {
+                List<Move> moves = board.getMovesFor(me);
+                return moves.get(random.nextInt(moves.size()));
+            }
+            return bestDepthMove;
         }
 
         private float run(Color color, int depth, float alpha, float beta)
         {
             if (!watchdog.call()) throw new TimeoutException();
-            if( depth == 0 ) {
-                Color winner = board.getWinner(color);
-                if (winner != null) {
-                    return winner == color ? 1 : -1;
-                }
-                return 0;
+            Color winner = board.getWinner(color);
+            if (winner != null) {
+                return winner == color ? Float.MAX_VALUE : -Float.MAX_VALUE;
             }
             List<Move> moves = board.getMovesFor(color);
+            if( depth == 0 ) {
+                float poll = 0;
+                for (int i = 0; i < 1 && watchdog.call(); i++) {
+                    poll += poller.poll(moves, board);
+                }
+                return color == me ? poll : -poll;
+            }
             for(Move move : moves) {
                 board.doMove(move);
                 float val = -run(getOpponent(color), depth - 1, -beta, -alpha);
                 board.undoMove(move);
-                if( val > alpha ) alpha = val; // alpha=max(val,alpha);
+                if( val > alpha ) {
+                    if (this.depth == depth) bestMove = move;
+                    alpha = val; // alpha=max(val,alpha);
+                }
                 if( alpha >= beta ) return beta; // cutoff
             } //endfor
             return alpha;
+        }
+
+        private class MovePoller {
+            private Board board;
+            private final Color me;
+            private int depth;
+            private int maxDepth;
+
+            public MovePoller(Color c) {
+                this.me = c;
+            }
+
+            public void setMaxDepth(int maxDepth) {
+                this.maxDepth = maxDepth;
+            }
+
+            public float poll(List<Move> moves, Board b) {
+                this.board = b;
+                maxDepth = Integer.MAX_VALUE;
+                depth = 0;
+                float winningWeight = canBeWinning(moves.get(random.nextInt(moves.size())), me);
+                if (winningWeight > 0 && depth < maxDepth) {
+                    maxDepth = depth;
+                }
+                return winningWeight / depth;
+            }
+
+            private int canBeWinning(Move move, Color current) {
+                if (depth >= maxDepth) return 0;
+                board.doMove(move);
+                depth++;
+                Color opponent = getOpponent(current);
+                Color winner = board.getWinner(opponent);
+                int foundWinning;
+                if (winner != null) {
+                    foundWinning = winner == me ? 1 : -100;
+                } else {
+                    List<Move> moves = board.getMovesFor(opponent);
+                    foundWinning = canBeWinning(moves.get(random.nextInt(moves.size())), opponent);
+                }
+                board.undoMove(move);
+                return foundWinning;
+            }
         }
     }
 }
